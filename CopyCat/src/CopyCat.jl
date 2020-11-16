56bce9f9e76feff80bcab3de224233d5807c27d0
radekBednarik <radek.bednarik@tesena.com>
2020-11-16 19:58:59 +0100

    init

---
+++ CopyCat/src/CopyCat.jl
@@ -0,0 +1,31 @@
+module CopyCat
+
+using ArgParse
+
+ParsedArgs = Dict{String, Any}
+
+function parse_cli_args()::ParsedArgs
+    settings::ArgParseSettings = ArgParseSettings()
+    @add_arg_table! settings begin
+        "--source", "-s"
+        help = "Dirpath to source folder."
+        arg_type = String
+        required = true
+
+        "--target", "-t"
+        help = "Dirpath to target folder."
+        arg_type = String
+        required = true
+    end
+
+    return ArgParse.parse_args(ARGS, settings)
+end
+
+function main()::Nothing
+    args::ParsedArgs = parse_cli_args()
+    println(args)
+end
+
+main()
+
+end # module
