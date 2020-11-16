a310422fe343779fedb39a05e79acb7c10e1debb
radekBednarik <radek.bednarik@tesena.com>
2020-11-16 20:52:19 +0100

    wip

--- CopyCat/src/CopyCat.jl
+++ CopyCat/src/CopyCat.jl
@@ -1,28 +1,36 @@
 module CopyCat
 
 using ArgParse
+using Base.Filesystem: abspath
 
 ParsedArgs = Dict{String, Any}
 
 function parse_cli_args()::ParsedArgs
-    settings::ArgParseSettings = ArgParseSettings()
+    settings = ArgParseSettings()
     @add_arg_table! settings begin
-        "--source", "-s"
+        "-s"
         help = "Dirpath to source folder."
-        arg_type = String
         required = true
 
-        "--target", "-t"
+        "-t"
         help = "Dirpath to target folder."
-        arg_type = String
         required = true
     end
 
-    return ArgParse.parse_args(ARGS, settings)
+    return parse_args(settings)
+end
+
+function convert_path_to_abs(path::String)::Union{String, Nothing}
+    try
+        return abspath(path)
+    catch e
+        println("Path $path could not be converted to absolute path.\n$(String(e))")
+        return nothing
+    end
 end
 
 function main()::Nothing
-    args::ParsedArgs = parse_cli_args()
+    a = parse_cli_args()
 end
 
 main()
